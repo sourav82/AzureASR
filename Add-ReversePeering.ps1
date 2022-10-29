05013abd4196357e57c57e05a64d3458a7aaa874
sourav82 <sourav82@users.noreply.github.com>
2022-10-29 23:16:48 +0100

    Update Add-ReversePeering.ps1

--- Add-ReversePeering.ps1
+++ Add-ReversePeering.ps1
@@ -1,2 +1,5 @@
-$vnetAlgo=Get-AzVirtualNetwork -Name Bermuda-VNET -ResourceGroupName BermudaRG
-Add-AzVirtualNetworkPeering -Name Bermuda2Algo -VirtualNetwork $vnetAlgo -RemoteVirtualNetworkId "/subscriptions/700d1aab-224e-4b10-8bdc-8d0835ba5213/resourceGroups/AlgoRG/providers/Microsoft.Network/virtualNetworks/AWA-VNET" -AllowForwardedTraffic
+$securePassword = echo $env:plainPassword | ConvertTo-SecureString -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:ApplicationId, $securePassword
+Connect-AzAccount -ServicePrincipal -TenantId $env:TenantIdBermuda -Credential $Credential
+$bermudaVnet=Get-AzVirtualNetwork -Name Bermuda-VNET -ResourceGroupName BermudaRG
+Add-AzVirtualNetworkPeering -Name Bermuda2Algo -VirtualNetwork $bermudaVnet -RemoteVirtualNetworkId "/subscriptions/700d1aab-224e-4b10-8bdc-8d0835ba5213/resourceGroups/AlgoRG/providers/Microsoft.Network/virtualNetworks/AWA-VNET" -AllowForwardedTraffic
